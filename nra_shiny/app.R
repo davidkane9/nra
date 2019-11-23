679ce0ce7b0976ba31fd20bd2c80e44534e548eb
David Kane <dave.kane@gmail.com>
2019-11-23 11:21:09 -0500

    bring over NavBar example from RStudio Shiny. Sadly, that requires a bunch of files to work, none of which are distributed!

--- nra_shiny/app.R
+++ nra_shiny/app.R
@@ -8,42 +8,68 @@
 #
 
 library(shiny)
+library(markdown)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+ui <- navbarPage("Navbar!",
+                 tabPanel("Plot",
+                          sidebarLayout(
+                              sidebarPanel(
+                                  radioButtons("plotType", "Plot type",
+                                               c("Scatter"="p", "Line"="l")
+                                  )
+                              ),
+                              mainPanel(
+                                  plotOutput("plot")
+                              )
+                          )
+                 ),
+                 tabPanel("Summary",
+                          verbatimTextOutput("summary")
+                 ),
+                 navbarMenu("More",
+                            tabPanel("Table",
+                                     DT::dataTableOutput("table")
+                            ),
+                            tabPanel("About",
+                                     fluidRow(
+                                         column(6,
+                                                includeMarkdown("about.md")
+                                         ),
+                                         column(3,
+                                                img(class="img-polaroid",
+                                                    src=paste0("http://upload.wikimedia.org/",
+                                                               "wikipedia/commons/9/92/",
+                                                               "1919_Ford_Model_T_Highboy_Coupe.jpg")),
+                                                tags$small(
+                                                    "Source: Photographed at the Bay State Antique ",
+                                                    "Automobile Club's July 10, 2005 show at the ",
+                                                    "Endicott Estate in Dedham, MA by ",
+                                                    a(href="http://commons.wikimedia.org/wiki/User:Sfoskett",
+                                                      "User:Sfoskett")
+                                                )
+                                         )
+                                     )
+                            )
+                 )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+server <- function(input, output, session) {
+        output$plot <- renderPlot({
+            plot(cars, type=input$plotType)
+        })
+        
+        output$summary <- renderPrint({
+            summary(cars)
+        })
+        
+        output$table <- DT::renderDataTable({
+            DT::datatable(cars)
+        })
+    }
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
