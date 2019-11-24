49cf40c11d484695ad4ccc4b9677db1cb1337fea
David Kane <dave.kane@gmail.com>
2019-11-24 16:03:13 -0500

    make the pdf public

--- nra_shiny/app.R
+++ nra_shiny/app.R
@@ -3,13 +3,19 @@
 library(shiny)
 
 ui <- navbarPage("NRA Conventions and Hospital Admissions",
-                 tabPanel("Plot"),
+                 tabPanel("Plot",
+                          downloadLink("downloadData", "Download")),
                  tabPanel("Story"),
                  tabPanel("About")
                 )
 
 
-server <- function(input, output, session) {}
+server <- function(input, output, session) {
+  
+  # output$downloadData <- downloadHandler(
+  #   filename = "nra.pdf")
+  
+}
 
 
 shinyApp(ui = ui, server = server)
---
+++ prep_shiny.R
@@ -0,0 +1,18 @@
+# Commends to issue to set up Shiny app. Don't really need a file, given that I
+# hope to only do this one time. But what if I make a new version of the PDF?
+# Then it is handy to recall what to do. Of course, the real purpose here is to
+# set a good example for students looking at this code. The rest of this project
+# may be sloppy, but this part is high quality!
+
+# Recall that the www directory is a special in a Shiny app. It is the only part
+# of the app that is "open" to the internet. In order to acces a file that is
+# placed there, you use:
+
+# https://davidkane9.shinyapps.io/nra_conventions/nra.pdf
+
+# Note that you don't need the www. The www justs tells Shiny to place anything
+# in that directory so it is accessible from the "top" of the app directory
+# structure.
+
+library(fs)
+file_copy("nra.pdf", "nra_shiny/www")
